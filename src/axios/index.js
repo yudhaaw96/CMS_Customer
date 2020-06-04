29affa0fd99caf7a430d72cdce7a119dc0f4a01e
yaw96_ <yaw.networks@gmail.com>
2020-06-04 12:21:51 +0700

    deploy

--- src/axios/index.js
+++ src/axios/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const url = 'http://localhost:3000'
-// const url = 'https://server-cms-ecommerce-yudhaaw96.herokuapp.com/'
+// const url = 'http://localhost:3000'
+const url = 'https://server-cms-ecommerce-yudhaaw96.herokuapp.com/'
 
 const axiosShot = axios.create({
   baseURL: url
